d3184da2a8893aaebcc852ffdb4bbeb73d729dcc
const <attwistingnether@gmail.com>
2019-04-21 22:14:31 +0200

    ..

--- cf.py
+++ cf.py
@@ -1,12 +1,13 @@
 from sklearn.naive_bayes import GaussianNB
 from numpy import genfromtxt
+import numpy as np
 
 
 my_data = genfromtxt('data.csv', delimiter=',')
 
-X = my_data
-y = [1, 1, 0, 0, 0, 0, 1, 0]
-
+X = my_data[:, 0:2]
+y = my_data[:, 2:]
+y = y.ravel()
 nb = GaussianNB()
 nb.fit(X, y)
-print(nb.predict([[153, 45]]))
+print(nb.predict([[183, 95]]))
